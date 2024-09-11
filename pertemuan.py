a3cc8eba56c523875570aad993b3dafcbc8845e1
figuran04 <dikaelsaputra2406@gmail.com>
2024-09-11 16:04:41 +0700

    edit: pertemuan.py

--- pertemuan.py
+++ pertemuan.py
@@ -14,12 +14,12 @@ def fetch_medium_post_summary(feed_url, post_link):
 
     return "Summary not found."
 
-def update_readme(summary):
+def update_readme(summary, readme_path, post_link):
     # Unescape HTML entities in the summary
     summary = unescape(summary)
 
     # Read the existing README content
-    with open('pertemuan-2/README.md', 'r', encoding='utf-8') as f:
+    with open(readme_path, 'r', encoding='utf-8') as f:
         readme_content = f.readlines()
 
     # Find the section to update
@@ -34,18 +34,24 @@ def update_readme(summary):
         if end_marker in line:
             end_idx = idx
 
-    # Prepare new content
-    new_content = f'{summary}\n'
+    # Prepare new content with URL and summary
+    new_content = f'[Baca di Medium]({post_link})\n\n{summary}\n'
 
     # If markers are found, replace the content in between
     if start_idx is not None and end_idx is not None:
         updated_content = readme_content[:start_idx + 1] + [new_content] + readme_content[end_idx:]
 
-    # Write the updated content back to README.md
-    with open('pertemuan-2/README.md', 'w', encoding='utf-8') as f:
-        f.writelines(updated_content)
+        # Write the updated content back to README.md
+        with open(readme_path, 'w', encoding='utf-8') as f:
+            f.writelines(updated_content)
 
 if __name__ == "__main__":
-    post_link = "https://medium.com/@dikaelsaputra/instalasi-dan-konfigurasi-hadoop-serta-spark-di-windows-f7f3582def93?source=rss-272e0aace4a6------2"
-    summary = fetch_medium_post_summary(FEED_URL, post_link)
-    update_readme(summary)
+    posts = [
+        ("https://medium.com/@dikaelsaputra/panduan-lengkap-pyspark-dan-pandas-instalasi-praktik-dasar-dan-lanjutan-86ab9ce4ea55?source=rss-272e0aace4a6------2", 'pertemuan-1/README.md'),
+        ("https://medium.com/@dikaelsaputra/instalasi-dan-konfigurasi-hadoop-serta-spark-di-windows-f7f3582def93?source=rss-272e0aace4a6------2", 'pertemuan-2/README.md'),
+        ("https://medium.com/@dikaelsaputra/implementasi-mapreduce-hadoop-sederhana-b413cfc67f5c?source=rss-272e0aace4a6------2", 'pertemuan-3/README.md')
+    ]
+    
+    for post_link, readme_path in posts:
+        summary = fetch_medium_post_summary(FEED_URL, post_link)
+        update_readme(summary, readme_path, post_link)
