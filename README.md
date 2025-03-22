579cf30e798bf764a24c449e0917bf118fac365f
figuran04 <dikaelsaputra2406@gmail.com>
2025-03-22 13:38:15 +0700

    fix: history

--- README.md
+++ README.md
@@ -9,12 +9,20 @@
     <th style="border: 1px solid white; padding: 10px;">Thumbnail</th>
   </tr>
   <tr>
-    <td style="border: 1px solid white; padding: 10px;"><h3><a href="https://medium.com/@dikaelsaputra/index-295589a56ed0?source=rss-272e0aace4a6------2" target="_blank" style="text-decoration: none;">Overview Pembelajaran Teknologi Informasi</a></h3><p>Menjelajahi Big Data, Java, dan Jaringan KomputerBlog ini merupakan ruang di mana saya berbagi wawas...</p></td>
-    <td style="border: 1px solid white; padding: 10px;"><img src="https://cdn-images-1.medium.com/max/641/1*452QjFn5ZxPn8QGRMTXXLw.png" alt="Post Image" style="width: 100px; height: auto;" /></td>
-  </tr>
-  <tr>
     <td style="border: 1px solid white; padding: 10px;"><h3><a href="https://medium.com/@dikaelsaputra/memprediksi-keberhasilan-tmdb-konten-tv-7b6826951b50?source=rss-272e0aace4a6------2" target="_blank" style="text-decoration: none;">Prediksi Keberhasilan TMDB &amp; Konten TV</a></h3><p>Prediksi Keberhasilan TMDB &amp; TV Shows: Logistic Regression &amp; Linear RegressionPada kesempatan kali i...</p></td>
     <td style="border: 1px solid white; padding: 10px;"><img src="https://cdn-images-1.medium.com/max/793/0*Qm_w9_yZFc8YE4H0" alt="Post Image" style="width: 100px; height: auto;" /></td>
+  </tr>
+  <tr>
+    <td style="border: 1px solid white; padding: 10px;"><h3><a href="https://medium.com/@dikaelsaputra/advanced-machine-learning-using-spark-mllib-1e5fe3e00ae2?source=rss-272e0aace4a6------2" target="_blank" style="text-decoration: none;">Advanced Machine Learning using Spark MLlib</a></h3><p>Dalam dunia big data dan komputasi terdistribusi, Apache Spark telah menjadi salah satu framework ya...</p></td>
+    <td style="border: 1px solid white; padding: 10px;"><img src="https://cdn-images-1.medium.com/max/815/1*im7-WrTtooYDAEV_Swc6TQ.png" alt="Post Image" style="width: 100px; height: auto;" /></td>
+  </tr>
+  <tr>
+    <td style="border: 1px solid white; padding: 10px;"><h3><a href="https://medium.com/@dikaelsaputra/data-cleaning-preparation-and-visualization-7ec75116b408?source=rss-272e0aace4a6------2" target="_blank" style="text-decoration: none;">Data Cleaning, Preparation, and Visualization</a></h3><p>Data Cleaning, Preparation, and Visualization: Pandas, Matplotlib, and SeabornData adalah inti dari ...</p></td>
+    <td style="border: 1px solid white; padding: 10px;"><img src="https://cdn-images-1.medium.com/max/827/1*wG3lr-PCgzqfsIAR95Q1cQ.png" alt="Post Image" style="width: 100px; height: auto;" /></td>
+  </tr>
+  <tr>
+    <td style="border: 1px solid white; padding: 10px;"><h3><a href="https://medium.com/@dikaelsaputra/advanced-mongodb-operations-and-data-query-6403253b25a4?source=rss-272e0aace4a6------2" target="_blank" style="text-decoration: none;">Advanced MongoDB Operations and Data Query</a></h3><p>MongoDB adalah salah satu database NoSQL yang paling populer, memberikan fleksibilitas, skalabilitas...</p></td>
+    <td style="border: 1px solid white; padding: 10px;"><img src="https://cdn-images-1.medium.com/max/686/1*YwH2L_Q7K8vy4FZ5Z6xMmQ.jpeg" alt="Post Image" style="width: 100px; height: auto;" /></td>
   </tr>
 </table>
 </div>
--- pertemuan.py
+++ pertemuan.py
@@ -1,5 +1,5 @@
 import feedparser
-from html import escape, unescape
+from html import unescape
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -16,20 +16,42 @@ def fetch_medium_post_summary(feed_url, post_link):
     
     for entry in feed.entries:
         if entry.link == post_link:
-            summary = entry.summary
-            return summary
+            return unescape(entry.summary)
+    
+    return None  # Jika tidak ditemukan, kembalikan None
 
-    return "Summary not found."
+def read_existing_summary(readme_path):
+    if not os.path.exists(readme_path):
+        return None
+
+    with open(readme_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    start_marker = "<!--START_SECTION:medium-->"
+    end_marker = "<!--END_SECTION:medium-->"
+    start_idx = content.find(start_marker)
+    end_idx = content.find(end_marker)
+
+    if start_idx != -1 and end_idx != -1:
+        old_summary = content[start_idx + len(start_marker):end_idx].strip()
+        return old_summary
+
+    return None
 
 def update_readme(summary, readme_path, post_link):
-    # Unescape HTML entities in the summary
-    summary = unescape(summary)
+    old_summary = read_existing_summary(readme_path)
 
-    # Read the existing README content
+    if summary is None:
+        summary = old_summary  # Gunakan ringkasan lama jika tidak ditemukan yang baru
+
+    if summary is None:
+        return  # Jika tidak ada ringkasan sebelumnya dan baru, tidak perlu update
+
+    # Baca isi README
     with open(readme_path, 'r', encoding='utf-8') as f:
         readme_content = f.readlines()
 
-    # Find the section to update
+    # Menentukan posisi untuk mengganti teks di dalam tag pembatas
     start_marker = "<!--START_SECTION:medium-->"
     end_marker = "<!--END_SECTION:medium-->"
     start_idx = None
@@ -41,26 +63,30 @@ def update_readme(summary, readme_path, post_link):
         if end_marker in line:
             end_idx = idx
 
-    # Prepare new content with URL and summary
-    new_content = f'[Baca di Medium]({post_link})\n\n{summary}\n'
+    # Buat konten baru
+    new_content = f"{start_marker}\n[Baca di Medium]({post_link})\n\n{summary}\n{end_marker}\n"
 
-    # If markers are found, replace the content in between
+    # Jika marker ditemukan, lakukan update
     if start_idx is not None and end_idx is not None:
-        updated_content = readme_content[:start_idx + 1] + [new_content] + readme_content[end_idx:]
+        updated_content = readme_content[:start_idx] + [new_content] + readme_content[end_idx + 1:]
 
-        # Write the updated content back to README.md
+        # Tulis ulang README.md
         with open(readme_path, 'w', encoding='utf-8') as f:
             f.writelines(updated_content)
 
 def fetch_post_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+
     thumbnail_url = soup.find('meta', {'property': 'og:image'})
     thumbnail_url = thumbnail_url['content'] if thumbnail_url else None
+
     publish_date = soup.find('span', {'data-testid': 'storyPublishDate'})
     publish = publish_date.text.strip() if publish_date else '0'
+
     readers_count = soup.find('span', {'data-testid': 'storyReadTime'})
     readers = readers_count.text.strip() if readers_count else '0'
+
     return thumbnail_url, publish, readers
 
 def create_post_image(post_data, index):
@@ -69,7 +95,7 @@ def create_post_image(post_data, index):
     draw = ImageDraw.Draw(image)
     font_size = 62
     font = ImageFont.truetype('Helvetica-Bold.ttf', font_size)
-    
+
     if post_data[0]:
         try:
             response = requests.get(post_data[0])
@@ -78,16 +104,14 @@ def create_post_image(post_data, index):
             image.paste(img, (0, 0))
         except Exception as e:
             print(f"Error loading image: {e}")
-     
+
     text_claps = [f"{post_data[2]}", f"{post_data[1]}"]
-    line_height = 10  
-    text_x = 0       
-    text_y = 415      
-    
+    text_x, text_y, line_height = 0, 415, 10  
+
     for line in text_claps:
         draw.text((text_x, text_y), line, font=font, fill='black')
         text_y += font_size + line_height  
-    
+
     output_path = os.path.join(output_folder, f'post_{index + 1}.png')
     image.save(output_path, "PNG")
 
